3dcbe689a031f98987899701a2a132d9d03c3b89
Charaneswar <32614346+Charaneswar@users.noreply.github.com>
2018-08-11 10:35:41 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -3,7 +3,10 @@ import matplotlib as plt
 import numpy as np
 import array as arr
 import dis
+#extracting full data to numpy array
 data=pd.read_csv('pre_primary_school_data.csv')
+data_array=data.values
+#
 male_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - M'].values
 female_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - F'].values
 schools=data['
