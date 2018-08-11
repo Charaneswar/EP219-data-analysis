63651b2ca120833e1e472554e6da80d951879483
Charaneswar <32614346+Charaneswar@users.noreply.github.com>
2018-08-11 11:43:41 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -10,8 +10,4 @@ data_array=data.values
 male_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - M'].values
 female_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - F'].values
 schools=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Number of Institutions'].values
-total_teachers=male_teachers+female_teachers
-teachers_per_school=np.zeros((108,1),float)
-i=0
-while(i<108)
 
