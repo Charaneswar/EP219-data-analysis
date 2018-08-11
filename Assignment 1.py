e6c988aca0028f1355362856f2ece00b94244035
Charaneswar <32614346+Charaneswar@users.noreply.github.com>
2018-08-11 10:46:07 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -6,7 +6,12 @@ import dis
 #extracting full data to numpy array
 data=pd.read_csv('pre_primary_school_data.csv')
 data_array=data.values
-#
+#extracting the needed columns into numpy array
 male_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - M'].values
 female_teachers=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Teachers - F'].values
-schools=data['
+schools=data['Pre - Primary Schools Having LKG / UKG / Nursery Classes - Number of Institutions'].values
+total_teachers=male_teachers+female_teachers
+teachers_per_school=np.zeros((108,1),float)
+i=0
+while(i<108)
+
