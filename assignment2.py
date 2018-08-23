1aaada9754771af4c6fe4f83c6024db570a773db
Charaneswar <32614346+Charaneswar@users.noreply.github.com>
2018-08-23 22:17:58 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import array as arr
+import dis
+import math
+data=pd.read_excel('swachhbharat.xlsx')
+data_array=data.values
+i=0
+odf_frac_UP=[]
+while(i<6871):
+	if(data_array[i,0]=='Uttar Pradesh'):
+		odf_frac_UP.append((data_array[i,5])/(data_array[i,4]))
+	i+=1
+j=odf_frac_UP.__len__()
+def mean_val(ar):
+	s=0
+	for i in range(0,j-1):
+		s=s+ar[i]
+	mn=s/j
+	return mn
+mn=mean_val(odf_frac_UP)
+def std_dev(ar):
+	s=0
+	for i in range(0,j-1):
+		s = s + pow((ar[i]-mn),2)
+	sd=math.sqrt(s/(j-1))
+	return sd
+sd=std_dev(odf_frac_UP)
+mn=mn-(mn%0.01)
+sd=sd-(sd%0.01)
+f1=plt.figure(1)
+tx1=f1.add_subplot(111)
+f1.subplots_adjust(top=0.85)
+tx1.text(0.5,100,r'Mean(Red)=%s'%(mn),fontsize=8)
+tx1.text(0.5,95,r'Standard Deviation(Green)=%s'%(sd),fontsize=8)
+plt.xlabel("Fraction of ODF Villages")
+plt.ylabel("Number of Districts")
+plt.title("Fraction of ODF Villages")
+plt.axvline(mn, color='r', label='Mean')
+plt.axvline(sd, color='g', label='Standard Deviation')
+plt.hist(odf_frac_UP,bins=15,histtype='bar',rwidth=0.7)
+plt.show()
